7ed52497db8979d8198aba1e7dcac6d78f94c109
Abdul khader Syed <syedak@adobe.com>
2021-03-21 20:33:38 -0700

    Added nvgpu package for tacking GPU mem

--- few_shot/utils.py
+++ few_shot/utils.py
@@ -6,6 +6,7 @@ from typing import Tuple, List
 from config import EPSILON, PATH
 
 import subprocess
+import sys
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
